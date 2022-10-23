98233265fb6502f5ff10e160516955470d9ee192
fmatoruga2 <fmatoruga2@etf.unsa.ba>
2022-10-23 09:07:45 +0200

    final begi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+
+
+    public static int faktorijel(int n){
+        int fakt=1;
+        for(int i=1;i<=n;i++)
+            fakt=fakt*i;
+        return fakt;
+    }
+
+    public static double sinus(int n){
+        double b=Math.toRadians(n);
+        return b;
+
+
+    }
+
+
     public static void main(String[] args) {
 
+        int n;
+        Scanner input= new Scanner(System.in);
+        int number= input.nextInt();
+        int faktorijel=faktorijel(number);
+        double sinus=sinus(number);
+
+
+        System.out.println("Faktorijel je: "+faktorijel+" ,sinus je: "+sinus);
     }
 }
